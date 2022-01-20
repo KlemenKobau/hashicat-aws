2fe7975bd05aaba070ca1417a6a179ce471d02ce
Klemen Kobau <klemen.kobau@gmail.com>
2022-01-20 12:09:28 +0000

    Private  s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "my-s3-bucket"
-  bucket_prefix = "kkobau"
-  acl = "private"
-  versioning = {
-    enabled = true
-  }
+module "s3-bucket" {
+  source = 
+"app.terraform.io/kk-workshop/s3-bucket/aws"
+  version = "2.13.0"
 }
